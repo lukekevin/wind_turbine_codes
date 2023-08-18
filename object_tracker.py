bfc9a7a535b7a52e66e9453e72bc045969725008
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2023-08-18 19:10:00 +0530

    Update object_tracker.py

--- object_tracker.py
+++ object_tracker.py
@@ -27,8 +27,7 @@ while True:
     cv2.imshow('Mask', mask)
     cv2.imshow('roi', roi)
 
-    key=cv2.waitKey(30)
-    if key==27:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
