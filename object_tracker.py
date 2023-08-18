46ee211d9b6ff0e8cf6d15f7faf5eb2ece547f68
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2023-08-18 18:12:07 +0530

    Update object_tracker.py

--- object_tracker.py
+++ object_tracker.py
@@ -1,29 +1,30 @@
 import cv2
+cap=cv2.VideoCapture('/home/kevin/object_tracker/highway.mp4')
 
-
-cap=cv2.VideoCapture('car.mp4 ')
-
-object_detector=cv2.createBackgroundSubtractorMOG2()
+object_detector=cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=60)
 
 while True:
     ret, frame=cap.read()
     height,width,_=frame.shape
-    print(height, width)
-
-    roi=frame[600:,1500:]
+    
+    roi=frame[258:715, 0:616]
 
     #object detection from mask
     mask=object_detector.apply(frame)
-    contours,_=cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE )
+    _,mask=cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    
+    contours,_=cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
     for cnt in contours:
         area=cv2.contourArea(cnt)
         if area>300:
-            cv2.drawContours(frame, [cnt], -1, (0,255,0), 2)
-
+            #cv2.drawContours(frame, [cnt], -1, (0,255,0), 2)
+            x,y,w,h=cv2.boundingRect(cnt)
+            cv2.rectangle(frame,(x,y),(x+w,y+h), (0,255,0),1 )
+            
 
 
     cv2.imshow("Frame", frame)
-    #cv2.imshow('Mask', mask)
+    cv2.imshow('Mask', mask)
     cv2.imshow('roi', roi)
 
     key=cv2.waitKey(30)
